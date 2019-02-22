7a07e520636f39e1e8d299b3abd11f02ecb89665
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2019-02-22 11:48:13 +0100

    readKnitrCache is now intelligent and asks for input

--- R/readKnitrCache.R
+++ R/readKnitrCache.R
@@ -1,8 +1,36 @@
 #' Read cached chunks from this .Rmd
 #'
 #' Read cached chunks from this .Rmd. All variables are loaded into the global environment.
-#' 
+#'
 #' @export
 readKnitrCache <- function() {
-   qwraps2::lazyload_cache_dir()
+
+   fullpath_document <- rstudioapi::getSourceEditorContext()$path
+
+   if (nchar(fullpath_document) == 0) {
+      stop('You need to save this document first.')
+   }
+
+   # Find the cache dir
+   path_document <- dirname(fullpath_document)
+   cache_dir_default <- './cache'
+
+   path_cache <- normalizePath(file.path(path_document, cache_dir_default), mustWork = FALSE)
+
+   while (!dir.exists(path_cache)) {
+
+      path_cache <- rstudioapi::selectDirectory(caption = 'Select cache folder', path = path_document)
+
+      # Abort clicked
+      if (is.null(path_cache)) {
+         return()
+      }
+
+   }
+
+   if (dir.exists(path_cache)) {
+      message(paste0('Found cache in ', path_cache))
+      qwraps2::lazyload_cache_dir(path_cache)
+   }
+
 }
